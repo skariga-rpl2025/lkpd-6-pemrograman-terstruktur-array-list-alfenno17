f66ef29477516e559359695703377746460e00bc
alfenno17 <alfenno1602@gmail.com>
2025-11-24 09:27:03 +0700

    commit

---
+++ src/Main.java
@@ -0,0 +1,40 @@
+ /*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Mutia
+ */
+public class Main {
+    public static void main(String[] args) {
+        
+        Perpustakaan perpus = new Perpustakaan();
+        perpus.tambahBuku("One it All","Raff");
+        perpus.tambahBuku("Mas Ambarawa", "Zrukis");
+        perpus.tambahBuku("Timur ke Utara", "Salim Bapak");
+        System.out.println("Jumlah Buku: " + perpus.jumlahBuku());
+        perpus.tambahBuku("Pray to Allah", "Umar");
+        
+        String cari = "Komedi";
+        System.out.println("Cari Buku dengan Kata Kunci: " + cari);
+        Buku buku = perpus.cariBuku(cari);
+        if(buku == null){
+            System.out.println("Buku Tidak Ditemukan! ");
+    
+        }else{
+            System.out.println("Buku ditemukan dengan judul: " + buku.getjudul() + 
+                    ", Pengarang: " + buku.getPengarang());
+        }
+        
+        perpus.gantiBuku(0, "Nasi Kotak", "Dono");
+        buku = perpus.lihatBuku(0);
+        System.out.println("Buku diganti dengan judul: " + buku.getjudul() +
+                ", Pengarang: " + buku.getPengarang());
+        
+        perpus.hapusBuku(0);
+        System.out.println("Jumlah Buku: " + perpus.jumlahBuku());
+    }
+    
+}
---
+++ src/Tugas/MainDataMahasiswa.java
@@ -0,0 +1,43 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Tugas;
+
+/**
+ *
+ * @author Mutia
+ */
+public class MainDataMahasiswa {
+    
+    public static void main(String[] args) {
+        
+        DataMahasiswa dataM = new DataMahasiswa();
+        
+        dataM.tambahMahasiswa("2214", "Agus", "Batu");
+        dataM.tambahMahasiswa("2215", "Ika", "Surabaya");
+        dataM.tambahMahasiswa("2216", "Dwi", "Samarinda");
+        dataM.tambahMahasiswa("2217", "Dava", "Ternate");
+        System.out.println("Jumlah Mahasiswa: " + dataM.jumlahMahasiswa());
+        dataM.tambahMahasiswa("2220", "Iqbal", "Surakarta");
+        
+        String cari = "2232";
+        System.out.println("Cari Mahasiswa dengan Npm: " + cari);
+        Mahasiswa mahas = dataM.cariMahasiswa(cari);
+        if(mahas == null){
+            System.out.println("Mahasiswa Tidak Ditemukan! ");
+    
+        }else{
+            System.out.println("Mahasiswa ditemukan dengan Npm: " + mahas.getNpm() + 
+                    ", Nama: " + mahas.getNama() + ", Alamat: " + mahas.getAlamat());
+        }
+        
+        dataM.gantiMahasiswa(0, "2245", "Hamid", "Samarinda");
+        mahas = dataM.lihatMahasiswa(0);
+        System.out.println("Mahasiswa diganti dengan Npm: " + mahas.getNpm() +
+                ", Nama: " + mahas.getNama() + ", Alamat: " + mahas.getAlamat());
+        
+        dataM.hapusMahasiswa(0);
+        System.out.println("Jumlah Mahasiswa: " + dataM.jumlahMahasiswa());
+    }
+}
